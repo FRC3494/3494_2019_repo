c4f70ef81dd49f211982aa3c563e30baea3a5f04
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-01-31 19:31:52 -0500

    Add some doc comments

--- src/main/java/frc/robot/sensors/PressureSensor.java
+++ src/main/java/frc/robot/sensors/PressureSensor.java
@@ -4,6 +4,9 @@ import edu.wpi.first.wpilibj.AnalogInput;
 
 public class PressureSensor {
     private AnalogInput ai;
+    /**
+     * The voltage into the sensor.
+     */
     private static final double VCC = 5.0;
 
     private PressureSensor(int inputPin) {
@@ -14,6 +17,12 @@ public class PressureSensor {
         return ai.getVoltage();
     }
 
+    /**
+     * Return the pressure, in PSI. Has a tolerance of 1.5% according to REV.
+     *
+     * @return Measured pressure, in PSI.
+     * @see "http://www.revrobotics.com/content/docs/REV-11-1107-DS.pdf"
+     */
     public double getPressure() {
         return (250 * (this.getVoltageOut() / VCC)) - 25;
     }
