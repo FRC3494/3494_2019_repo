00cf64c444c386523a1798d57c085b0e8839d101
Caleb Berger <caleb.x.berger@gmail.com>
2019-02-06 17:13:32 -0500

    Transition CargoManipulator to TalonSRX controllers

--- src/main/java/frc/robot/subsystems/CargoManipulator.java
+++ src/main/java/frc/robot/subsystems/CargoManipulator.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
-
 
 public class CargoManipulator extends Subsystem {
     private static final CargoManipulator INSTANCE = new CargoManipulator();
@@ -12,25 +12,22 @@ public class CargoManipulator extends Subsystem {
         return INSTANCE;
     }
 
-    private CANSparkMax rightMotor;
-    private CANSparkMax leftMotor;
+    private TalonSRX rightMotor;
+    private TalonSRX leftMotor;
 
     private CargoManipulator() {
-
-        this.rightMotor = new CANSparkMax(RobotMap.CARGO_MANIPULATOR.RIGHT_MOTOR_CHANNEL, CANSparkMax.MotorType.kBrushless);
-        this.leftMotor = new CANSparkMax(RobotMap.CARGO_MANIPULATOR.LEFT_MOTOR_CHANNEL, CANSparkMax.MotorType.kBrushless);
-
+        this.rightMotor = new TalonSRX(RobotMap.CARGO_MANIPULATOR.RIGHT_MOTOR_CHANNEL);
+        this.leftMotor = new TalonSRX(RobotMap.CARGO_MANIPULATOR.LEFT_MOTOR_CHANNEL);
     }
 
     public void drive(double speed) {
-        this.leftMotor.set(speed);
-        this.rightMotor.set(speed);
+        this.leftMotor.set(ControlMode.PercentOutput, speed);
+        this.rightMotor.set(ControlMode.PercentOutput, speed);
     }
 
     public void stop() {
         this.drive(0);
     }
-
 
     public void initDefaultCommand() {
         // TODO: Set the default command, if any, for a subsystem here. Example:
