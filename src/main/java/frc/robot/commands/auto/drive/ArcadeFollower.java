b418039a6fd5d899cdd4a85a7972237239e00782
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-02-23 13:40:53 -0500

    Add ArcadeFollower command Nothing special, just a rip-off of 2017 code. See https://www.youtube.com/watch?v=zcSkjtpUT84 for a video demo.

---
+++ src/main/java/frc/robot/commands/auto/drive/ArcadeFollower.java
@@ -0,0 +1,39 @@
+package frc.robot.commands.auto.drive;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.OI;
+import frc.robot.Robot;
+import frc.robot.sensors.NavX;
+import frc.robot.subsystems.Drivetrain;
+
+public class ArcadeFollower extends Command {
+
+    private double velo;
+
+    public ArcadeFollower() {
+        this(0.5);
+    }
+
+    public ArcadeFollower(double speed) {
+        requires(Drivetrain.getInstance());
+        velo = speed;
+    }
+
+    @Override
+    protected void initialize() {
+        // Enable PID controller, just in case
+        Drivetrain.getInstance().enable();
+    }
+
+    @Override
+    protected void execute() {
+        Drivetrain.getInstance().setSetpoint(NavX.getInstance().getFusedHeading() + Robot.getLimelight().getXDistance());
+        double tune = Drivetrain.getInstance().getPidOutput();
+        Drivetrain.getInstance().tankDrive(velo + tune, velo - tune);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return !OI.getInstance().cruiseControlCancel();
+    }
+}
