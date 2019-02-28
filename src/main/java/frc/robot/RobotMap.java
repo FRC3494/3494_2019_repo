e86082d2ed88c99707aa01bae581fc5e97b64210
Caleb Berger <caleb.x.berger@gmail.com>
2019-02-27 19:28:37 -0500

    Change mappings to saner values

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -80,8 +80,8 @@ public class RobotMap {
         public static final int ZBAR_ENGAGE_BUTTON = 5;
         public static final int ZBAR_DISENGAGE_BUTTON = 11;
 
-        public static final int ENGAGE_REAR_FEET = 2;
-        public static final int DISENGAGE_ALL_FEET = 3;
+        public static final int ENGAGE_REAR_FEET = 6;
+        public static final int DISENGAGE_ALL_FEET = 7;
 
         public static final int EXTEND_HATCHER = 1; // A
         public static final int RETRACT_HATCHER = 2; // B
