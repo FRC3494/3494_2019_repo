1ad985bbf92525660bd65340c687ba0bdf12990a
Caleb Berger <caleb.x.berger@gmail.com>
2019-03-16 15:58:10 -0400

    Recreate GotoPosition
    I really shouldn't have deleted this... whoops.

---
+++ src/main/java/frc/robot/commands/arm/GotoPosition.java
@@ -0,0 +1,34 @@
+package frc.robot.commands.arm;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.CargoManipulatorArm;
+
+public class GotoPosition extends Command {
+
+    private double angle;
+
+    public GotoPosition(double angle) {
+        this.angle = angle;
+    }
+
+    @Override
+    protected void initialize() {
+        CargoManipulatorArm.getInstance().enable();
+        CargoManipulatorArm.getInstance().setSetpoint(angle);
+    }
+
+    @Override
+    protected void execute() {
+        CargoManipulatorArm.getInstance().lift(CargoManipulatorArm.getInstance().getPidOut());
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return CargoManipulatorArm.getInstance().onTarget();
+    }
+
+    @Override
+    protected void end() {
+        CargoManipulatorArm.getInstance().disable();
+    }
+}
