7ed778a38b3d1f40c9549d56ce09c9c5c38989d4
Caleb Berger <caleb.x.berger@gmail.com>
2019-03-16 15:57:46 -0400

    Implement PIDSubsystem on the arm
    Not sure if this is the best route due to the notorious loop overruns this year's WPILIb seems to suffer from. If it becomes an issue we can always use the on-Talon control (though this will cause electrical grief)

--- src/main/java/frc/robot/subsystems/CargoManipulatorArm.java
+++ src/main/java/frc/robot/subsystems/CargoManipulatorArm.java
@@ -4,7 +4,7 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 import frc.robot.RobotMap;
 import frc.robot.commands.arm.TwistArm;
@@ -12,7 +12,7 @@ import frc.robot.commands.arm.TwistArm;
 /**
  * Arm subsystem. In the context of this class, "rotations" refers to rotations of the arm gearbox output shaft.
  */
-public class CargoManipulatorArm extends Subsystem {
+public class CargoManipulatorArm extends PIDSubsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
@@ -22,7 +22,15 @@ public class CargoManipulatorArm extends Subsystem {
 
     private Potentiometer pot;
 
+    private double pidOut;
+
     private CargoManipulatorArm() {
+        super(0.125, 0, 0);
+        this.getPIDController().setContinuous(false);
+        this.setInputRange(0, 180);
+        this.setOutputRange(-0.5, 0.5);
+        this.setPercentTolerance(1.0);
+
         armMotor = new TalonSRX(RobotMap.CARGO_ARM.ARM_MOTOR_CHANNEL);
 
         diskBrake = new DoubleSolenoid(RobotMap.PCM_A, RobotMap.CARGO_ARM.DISK_BRAKE_FORWARD, RobotMap.CARGO_ARM.DISK_BRAKE_REVERSE);
@@ -52,6 +60,20 @@ public class CargoManipulatorArm extends Subsystem {
     public void initDefaultCommand() {
         // TODO: Set the default command, if any, for a subsystem here. Example:
         setDefaultCommand(new TwistArm());
+    }
+
+    @Override
+    protected double returnPIDInput() {
+        return this.pot.get();
+    }
+
+    @Override
+    protected void usePIDOutput(double output) {
+        this.pidOut = output;
+    }
+
+    public double getPidOut() {
+        return pidOut;
     }
 }
 
