6e9e63269a996c80b04953e519f49c18b8b5c9d0
Katrina <butterfly5748@gmail.com>
2019-04-03 18:26:17 -0400

    moved correction_factor constant to RobotMap insttead from Drive

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -43,6 +43,7 @@ public class RobotMap {
 
         public static final double MIN_CORRECTION_FACTOR = .1;
         public static final double MAX_CORRECTION_FACTOR = .4;
+        public static final double CORRECTION_FACTOR = (MAX_CORRECTION_FACTOR - MIN_CORRECTION_FACTOR) / (45 - PITCH_THRESHOLD_DEGREES);
     }
 
     public class CLIMBER {
